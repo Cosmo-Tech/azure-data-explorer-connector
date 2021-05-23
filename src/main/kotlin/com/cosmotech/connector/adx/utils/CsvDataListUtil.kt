d195406985bb4c8b7602db81afb13334120e5f5a
Vincent Carluer <vcarluer@gmail.com>
2021-05-24 00:04:33 +0200

    replace @, readline directly and debug file content

--- src/main/kotlin/com/cosmotech/connector/adx/utils/CsvDataListUtil.kt
+++ src/main/kotlin/com/cosmotech/connector/adx/utils/CsvDataListUtil.kt
@@ -15,23 +15,29 @@ class CsvDataListUtil {
           file.name.endsWith(".csv") }
         .forEach { file ->
           LOGGER.info("File: ${file}")
-          val header: String? = file.bufferedReader()
-            .useLines { lines: Sequence<String> ->
-                    lines.firstOrNull()
-                }
-          LOGGER.info("Header line:${header}")
-          val headers = if (header != null) header.split(",") else listOf()
-          // Filename will be used as table name so in fact it is file witout extension we want here
-          val cols: MutableMap<String, String> = headers.associateWith { "string" }.toMutableMap<String, String>()
-          val csvData = CsvData(
-            fileName = file.nameWithoutExtension,
-            headers = cols,
-            rows = mutableListOf(mutableListOf()),
-            exportDirectory = "",
-          )
+          val data = file.readText(Charsets.UTF_8)
+          LOGGER.info("file content:")
+          LOGGER.info(data)
+          var header: String? = file.bufferedReader().readLine()
+          if (header == null) {
+            LOGGER.warning("Empty file ${file.path}")
+          } else {
+            // Replace specific Cosmo Tech prefix
+            header = header.replace("@","")
+            LOGGER.info("Header line:${header}")
+            val headers = if (header != null) header.split(",") else listOf()
+            // Filename will be used as table name so in fact it is file witout extension we want here
+            val cols: MutableMap<String, String> = headers.associateWith { "string" }.toMutableMap<String, String>()
+            val csvData = CsvData(
+              fileName = file.nameWithoutExtension,
+              headers = cols,
+              rows = mutableListOf(mutableListOf()),
+              exportDirectory = "",
+            )
 
-          LOGGER.info("Add ${file.path} to ${csvData.toString()}")
-          datas.put(file.path, csvData)
+            LOGGER.info("Add ${file.path} to ${csvData.toString()}")
+            datas.put(file.path, csvData)
+          }
         }
       }
     }
